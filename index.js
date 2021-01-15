72c8f304ee4003caa95609287eb60c69d23afe13
Karthikraja Kannan <karthikraja.kannan@skill-mine.com>
2021-01-15 22:43:13 +0530

    fix: cors issue

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-const http = require('http');
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    port = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-    if(req.url == '/') {
-        res.write('Hello World');
-        res.end();
-    }
-
-    if(req.url === '/api/person') {
-        res.write(JSON.stringify({success: true, status: 200, data: { name: "karthikraja", mail: "karthikraja1994@hotmail.com" }}))
-        res.end();
-    }
+// enable CORS without external module
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-server.listen(5000);
+// parse application/json
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-console.log('Listening on 5000');
+
+// request handlers
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello World' });
+});
+
+// request handlers
+app.get('/api/person', (req, res) => {
+    res.status(200).json({ success: true, status: 200, data: { name: "karthikraja", mail: "karthikraja1994@hotmail.com" } });
+});
+
+app.listen(port, () => {
+    console.log('Server started on: ' + port);
+});
